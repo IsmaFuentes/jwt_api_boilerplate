14e94baf9e125526882d00dceb4961469a61ad51
Ismael Fuentes <isma.fuentes@kuarasoftware.com>
2021-05-17 10:10:32 +0200

    Api service + interfaces

---
+++ jwt_api_boilerplate/Interfaces/IService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace jwt_api_boilerplate.Interfaces
+{
+    public interface IService<T>
+    {
+        Task<T> Get(int? Oid);
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Create(T element);
+        Task<T> Update(T element);
+        Task<T> Delete(int? Oid);
+    }
+}
---
+++ jwt_api_boilerplate/Services/APIService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using jwt_api_boilerplate.Interfaces;
+using jwt_api_boilerplate.Models;
+
+namespace jwt_api_boilerplate.Services
+{
+    public sealed class APIService<T> : IService<T> where T : class
+    {
+        private readonly Context ctx;
+
+        public APIService(Context ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public async Task<T> Get(int? id)
+        {
+            return await ctx.FindAsync<T>(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await ctx.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> Create(T obj)
+        {
+            ctx.Set<T>().Add(obj);
+            await ctx.SaveChangesAsync();
+
+            return obj;
+        }
+
+        public async Task<T> Update(T obj)
+        {
+            ctx.Entry(obj).State = EntityState.Modified;
+            await ctx.SaveChangesAsync();
+
+            return obj;
+        }
+
+        public async Task<T> Delete(int? id)
+        {
+            T obj = await Get(id);
+
+            ctx.Entry(obj).State = EntityState.Deleted;
+            await ctx.SaveChangesAsync();
+
+            return obj;
+        }
+    }
+}
